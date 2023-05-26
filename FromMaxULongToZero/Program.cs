7559f1ae307d0e57aef8211e4e8771b7a72c3777
Marcos <Marcos@DESKTOP-1IOFE6L>
2023-05-26 12:38:40 -0300

    Agregar archivos de proyecto.

---
+++ FromMaxULongToZero/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FromMaxULongToZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong maxLong = UInt64.MaxValue;
+            int operationsDone = 0;
+            Console.WriteLine(maxLong);
+            do
+            {
+                maxLong = (maxLong % 2 == 0) ? maxLong/2 : maxLong-1 ;
+                operationsDone++;
+                Console.WriteLine(maxLong);
+            } while (maxLong != 0);
+            Console.WriteLine("Number of operations done over original number:" + operationsDone);
+        }
+    }
+}
